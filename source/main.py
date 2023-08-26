a0d5f3e0f2d19450c6a5882944d9ab9b5713ac61
FE <93105911+ugokutennp@users.noreply.github.com>
2023-08-26 22:31:00 +0900

    v1.4

--- source/main.py
+++ source/main.py
@@ -1,9 +1,8 @@
-import tkinter as tk
 import customtkinter as ctk
-import re
 import subprocess
-import time
 import threading
+import time
+import re
 
 class SidebarFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -11,7 +10,7 @@ class SidebarFrame(ctk.CTkFrame):
 
         self.sidebar_label_logo = ctk.CTkLabel(self, text="Pairing Assistant", font=ctk.CTkFont(size=20, weight="bold"))
         self.sidebar_label_logo.grid(row=0, column=0, padx=13, pady=(30, 20))
-        self.sidebar_button_reload = ctk.CTkButton(self,text="Reload",command=self.sidebar_button_reload_callbacks, border_width=2)
+        self.sidebar_button_reload = ctk.CTkButton(self,text="Reload", border_width=2,command=self.sidebar_button_reload_callback)
         self.sidebar_button_reload.grid(row=1, column=0, padx=20, pady=10)
         self.sidebar_button_pairall = ctk.CTkButton(self,text="PairAll",command=lambda:self.sidebar_button_callback("pairall"))
         self.sidebar_button_pairall.grid(row=2, column=0, padx=20, pady=10)
@@ -19,7 +18,8 @@ class SidebarFrame(ctk.CTkFrame):
         self.sidebar_button_force_pairall.grid(row=3, column=0, padx=20, pady=10)
         self.sidebar_button_unpairall = ctk.CTkButton(self,text="UnpairAll",command=lambda:self.sidebar_button_callback("unpairall"))
         self.sidebar_button_unpairall.grid(row=4, column=0, padx=20, pady=10)
-        self.sidebar_button_unpairall = ctk.CTkButton(self,text="DongleResetAll",command=lambda:self.sidebar_button_callback("dongleresetall"),fg_color="transparent", border_width=2,text_color=("gray10", "#DCE4EE"),)
+        self.sidebar_button_unpairall = ctk.CTkButton(self,text="DongleResetAll",fg_color="transparent", border_width=2,text_color=("gray10", "#DCE4EE"),
+                                                      command=lambda:self.sidebar_button_callback("dongleresetall"))
         self.sidebar_button_unpairall.grid(row=5, column=0, padx=20, pady=10)
 
         self.sidebar_label_mode = ctk.CTkLabel(self, text="Theme:")
@@ -30,7 +30,7 @@ class SidebarFrame(ctk.CTkFrame):
         #reset
         self.sidebar_optionemenu_mode.set("System")
 
-    def sidebar_button_reload_callbacks(self):
+    def sidebar_button_reload_callback(self):
         app_instance = self.master
         exe_path = app_instance.exe_path_frame.exepath_entry_serial.get()
         output = app_instance.execute_subprocess("serial", exe_path)
@@ -38,7 +38,7 @@ class SidebarFrame(ctk.CTkFrame):
         app_instance.insert_log("Recognized devices : " + ", ".join(device_serials))
 
         app_instance.scrollable_frame.update_device_frames(device_serials,app_instance)
-        app_instance.status_check()
+        app_instance.check_status()
         app_instance.insert_log("Reloaded devices")
         
     def sidebar_button_callback(self, command):
@@ -57,7 +57,7 @@ class SidebarFrame(ctk.CTkFrame):
         elif command == "forcepairall":
             self.after(5000, lambda: self.sidebar_button_force_pairall.configure(text="ForcePairAll",state="normal"))
 
-        self.after(5000, app_instance.status_check)
+        self.after(5000, app_instance.check_status)
         app_instance.insert_log("Executed command: " + command)
 
     def sidebar_optionemenu_callback(self, new_appearance_mode: str):
@@ -98,11 +98,12 @@ class DeviceFrame(ctk.CTkFrame):
         #self.device_label_name.grid(row=0, column=2, padx=20, pady=20)
         self.device_label_name.place(x=210,y=20)
 
-        self.device_button_pair = ctk.CTkButton(self,width=100,command=lambda: self.device_button_callback("pair",serial), text="Pair")
+        self.device_button_pair = ctk.CTkButton(self,text="Pair",width=100,command=lambda: self.device_button_callback("pair",serial))
         self.device_button_pair.grid(row=0, column=4, padx=(70, 10), pady=20)
-        self.device_button_unpair = ctk.CTkButton(self,width=100,command=lambda: self.device_button_callback("unpair",serial), text="Unpair")
+        self.device_button_unpair = ctk.CTkButton(self,text="Unpair",width=100,command=lambda: self.device_button_callback("unpair",serial))
         self.device_button_unpair.grid(row=0, column=5, padx=(10, 10), pady=20)
-        self.device_button_reset = ctk.CTkButton(self,width=100, text_color=("gray10", "#DCE4EE"), fg_color="transparent", border_width=2, command=lambda: self.device_button_callback("donglereset",serial), text="DongleReset")
+        self.device_button_reset = ctk.CTkButton(self,text="DongleReset",width=100, text_color=("gray10", "#DCE4EE"), fg_color="transparent", border_width=2, 
+                                                 command=lambda: self.device_button_callback("donglereset",serial))
         self.device_button_reset.grid(row=0, column=6, padx=(10, 20), pady=20)
 
     def device_button_callback(self,command,serial):
@@ -117,9 +118,9 @@ class DeviceFrame(ctk.CTkFrame):
         if command == "pair":
             self.after(5000, lambda: self.device_button_pair.configure(text="Pair",state="normal"))
         
-        self.after(5000, self.app_instance.status_check)
+        self.after(5000, self.app_instance.check_status)
 
-    def button_status_change(self, status):
+    def change_button_status(self, status):
         if status == "normal":
             self.device_button_pair.configure(state=status, text="Pair")
         elif status == "disabled":
@@ -131,22 +132,22 @@ class ExePathFrame(ctk.CTkFrame):
 
         self.exepath_entry_serial = ctk.CTkEntry(self, width=120)
         self.exepath_entry_serial.grid(row=0, column=0, padx=(20, 0), pady=(20, 20), sticky="ew")
-
-        self.exepath_button_browse = ctk.CTkButton(self, command=self.exepath_button_callback, fg_color="transparent", text_color=("gray10", "#DCE4EE"), border_width=2, text="Browse")
+        self.exepath_button_browse = ctk.CTkButton(self,text="Browse",fg_color="transparent", text_color=("gray10", "#DCE4EE"), border_width=2,
+                                                   command=self.exepath_button_callback)
         self.exepath_button_browse.grid(row=0, column=1, padx=20, pady=20)
 
         #reset
-        self.exepath_entry_serial.delete(0, tk.END)
+        self.exepath_entry_serial.delete(0, ctk.END)
         self.exepath_entry_serial.insert(0, r"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\tools\lighthouse\bin\win64\lighthouse_console.exe")
 
     def exepath_button_callback(self):
         app_instance = self.master
-        file_name = app_instance.file_read()
+        file_name = app_instance.read_file()
 
         if file_name is not None:
-            self.exepath_entry_serial.delete(0, tk.END)
+            self.exepath_entry_serial.delete(0, ctk.END)
             self.exepath_entry_serial.insert(0, file_name)
-            app_instance.exe_check()
+            threading.Thread(target=app_instance.check_exe).start()
                   
 class App(ctk.CTk):
     def __init__(self):
@@ -178,14 +179,14 @@ class App(ctk.CTk):
         self.textbox_log.grid(row=2, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         #reset
-        self.insert_log("Welcome to watchman_pairing_assistant ! (v1.3)")
-        self.exe_check()
-        self.sidebar_frame.sidebar_button_reload_callbacks()
+        self.insert_log("Welcome to watchman_pairing_assistant ! (v1.4)")
+        self.check_exe()
+        self.sidebar_frame.sidebar_button_reload_callback()
         
     def insert_log(self,log):
         current_time = time.strftime("[%Y-%m-%d %H:%M:%S] ")
-        self.textbox_log.insert(tk.END, current_time + log + "\n")
-        self.textbox_log.see(tk.END)
+        self.textbox_log.insert(ctk.END, current_time + log + "\n")
+        self.textbox_log.see(ctk.END)
         print(current_time + log)
 
     def execute_subprocess(self, command, exe_path):
@@ -209,26 +210,30 @@ class App(ctk.CTk):
             if process.poll() is None:
                 process.wait()
 
-    def status_check(self):
+    def check_status(self):
         exe_path = self.exe_path_frame.exepath_entry_serial.get()
-        output = self.execute_subprocess("dongleinfo", exe_path)
-        #print(output)
 
-        for device_frame in self.scrollable_frame.device_frames:
-            serial = device_frame.device_label_serial.cget("text")
-            serial_appears_disabled = f"VRC-{serial}" in output
+        def check_status_thread():
+            output = self.execute_subprocess("dongleinfo", exe_path)
+            #print(output)
 
-            if serial_appears_disabled:
-                self.device_status_change(serial,"disabled")
-                self.insert_log("Device connected with "+serial)
-            else:
-                self.device_status_change(serial,"normal")
+            for device_frame in self.scrollable_frame.device_frames:
+                serial = device_frame.device_label_serial.cget("text")
+                serial_appears_disabled = f"VRC-{serial}" in output
 
-    def device_status_change(self, serial, status):
+                if serial_appears_disabled:
+                    self.change_device_status(serial,"disabled")
+                    self.insert_log("Device connected with "+serial)
+                else:
+                    self.change_device_status(serial,"normal")
+
+        threading.Thread(target=check_status_thread).start()
+
+    def change_device_status(self, serial, status):
         for device_frame in self.scrollable_frame.device_frames:
             check_serial = device_frame.device_label_serial.cget("text")
             if check_serial == serial:
-                device_frame.button_status_change(status)
+                device_frame.change_button_status(status)
                 break
 
     def extract_device_serials(self, output):
@@ -245,17 +250,18 @@ class App(ctk.CTk):
         else:
             return "Dongle"
         
-    def exe_check(self):
-        exe_path = self.exe_path_frame.exepath_entry_serial.get() 
+    def check_exe(self):
+        exe_path = self.exe_path_frame.exepath_entry_serial.get()
         output = self.execute_subprocess("serial", exe_path)
+        #print(output)
 
         if "lighthouse_console.exe" in output:
             self.insert_log("Successfully recognized lighthouse_console.exe")
         else:
             self.insert_log("Failed to recognize lighthouse_console.exe")
 
-    def file_read(self):
-        file_path = tk.filedialog.askopenfilename(filetypes=[("Executable Files", "*.exe")])
+    def read_file(self):
+        file_path = ctk.filedialog.askopenfilename(filetypes=[("Executable Files", "*.exe")])
 
         if len(file_path) != 0:
             return file_path
