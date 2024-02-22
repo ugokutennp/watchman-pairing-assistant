cd125152e26e6981526f7d3038a5e47ca536314a
FE <93105911+ugokutennp@users.noreply.github.com>
2024-02-22 10:59:24 +0900

    V2

--- source/main.py
+++ source/main.py
@@ -3,36 +3,34 @@ import subprocess
 import threading
 import time
 import re
+import json
+import os
+import sys
 
 class SidebarFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):#Widget Placement
+    def __init__(self, master, app_instance, **kwargs):#Widget Placement
         super().__init__(master, width=140, corner_radius=0, **kwargs)
+        self.app_instance = app_instance
 
-        self.sidebar_label_logo = ctk.CTkLabel(self, text="Pairing Assistant", font=ctk.CTkFont(size=20, weight="bold"))
-        self.sidebar_label_logo.grid(row=0, column=0, padx=13, pady=(30, 20))
-        self.sidebar_button_reload = ctk.CTkButton(self,text="Reload", border_width=2,command=self.sidebar_button_reload_callback)
-        self.sidebar_button_reload.grid(row=1, column=0, padx=20, pady=10)
-        self.sidebar_button_pairall = ctk.CTkButton(self,text="PairAll",command=lambda:self.sidebar_button_callback("pairall"))
-        self.sidebar_button_pairall.grid(row=2, column=0, padx=20, pady=10)
-        self.sidebar_button_force_pairall = ctk.CTkButton(self,text="ForcePairAll",command=lambda:self.sidebar_button_callback("forcepairall"))
-        self.sidebar_button_force_pairall.grid(row=3, column=0, padx=20, pady=10)
-        self.sidebar_button_unpairall = ctk.CTkButton(self,text="UnpairAll",command=lambda:self.sidebar_button_callback("unpairall"))
-        self.sidebar_button_unpairall.grid(row=4, column=0, padx=20, pady=10)
-        self.sidebar_button_unpairall = ctk.CTkButton(self,text="DongleResetAll",fg_color="transparent", border_width=2,text_color=("gray10", "#DCE4EE"),
-                                                      command=lambda:self.sidebar_button_callback("dongleresetall"))
+        self.sidebar_label_logo1 = ctk.CTkLabel(self, text="Watchman", font=("Arial Bold", 20),text_color=("#696969", "#DCE4EE"))
+        self.sidebar_label_logo1.grid(row=0, column=0, padx=13, pady=(27, 0), sticky="w")
+        self.sidebar_label_logo2 = ctk.CTkLabel(self, text="Pairing Assistant", font=("Arial Bold", 20),text_color=("#696969", "#DCE4EE"))
+        self.sidebar_label_logo2.grid(row=1, column=0, padx=13, pady=(0, 27))
+        self.sidebar_button_reload = ctk.CTkButton(self,text="Reload", font=("Arial Bold", 12), fg_color=("#a9a9a9","#808080"),hover_color=("#696969","#696969"), command=self.sidebar_button_reload_callback)
+        self.sidebar_button_reload.grid(row=2, column=0, padx=20, pady=10)
+        self.sidebar_button_pairall = ctk.CTkButton(self,text="PairAll", font=("Arial Bold", 12),command=lambda:self.sidebar_button_callback("pairall"))
+        self.sidebar_button_pairall.grid(row=3, column=0, padx=20, pady=10)
+        self.sidebar_button_force_pairall = ctk.CTkButton(self,text="ForcePairAll", font=("Arial Bold", 12),command=lambda:self.sidebar_button_callback("forcepairall"))
+        self.sidebar_button_force_pairall.grid(row=4, column=0, padx=20, pady=10)
+        self.sidebar_button_unpairall = ctk.CTkButton(self,text="UnpairAll", font=("Arial Bold", 12),command=lambda:self.sidebar_button_callback("unpairall"))
         self.sidebar_button_unpairall.grid(row=5, column=0, padx=20, pady=10)
-
-        self.sidebar_label_mode = ctk.CTkLabel(self, text="Theme:")
-        self.sidebar_label_mode.grid(row=7, column=0, padx=20, pady=(10, 0))
-        self.sidebar_optionemenu_mode = ctk.CTkOptionMenu(self, values=["Light", "Dark", "System"],command=self.sidebar_optionemenu_callback)
-        self.sidebar_optionemenu_mode.grid(row=8, column=0, padx=20, pady=(10, 20))
-
-        #reset
-        self.sidebar_optionemenu_mode.set("System")
+        self.sidebar_button_dongleresetall = ctk.CTkButton(self,text="DongleResetAll", font=("Arial Bold", 12),fg_color="transparent",hover_color=("#696969","#808080"), border_width=2,border_color=("#a9a9a9","#c0c0c0"),text_color=("#696969", "#DCE4EE"),
+                                                      command=lambda:self.sidebar_button_callback("dongleresetall"))
+        self.sidebar_button_dongleresetall.grid(row=6, column=0, padx=20, pady=10)
 
     def sidebar_button_reload_callback(self):#Function to reload device
         app_instance = self.master
-        exe_path = app_instance.exe_path_frame.exepath_entry_serial.get()
+        exe_path = self.app_instance.get_exe_path()
         output = app_instance.execute_subprocess("serial", exe_path)
         device_serials = app_instance.extract_device_serials(output)
         if device_serials:
@@ -45,7 +43,14 @@ class SidebarFrame(ctk.CTkFrame):
         
     def sidebar_button_callback(self, command):#Processing when a button on the sidebar is pressed
         app_instance = self.master
-        exe_path = app_instance.exe_path_frame.exepath_entry_serial.get() 
+        exe_path = self.app_instance.get_exe_path() 
+        
+        def delayed_function():#Process to be executed after 5 seconds
+            app_instance.check_status()
+            if command == "pairall":
+                self.sidebar_button_pairall.configure(text="PairAll", state="normal")
+            elif command == "forcepairall":
+                self.sidebar_button_force_pairall.configure(text="ForcePairAll", state="normal")
     
         if command == "pairall":
             self.sidebar_button_pairall.configure(text="Pairing...",state="disabled")
@@ -53,17 +58,9 @@ class SidebarFrame(ctk.CTkFrame):
             self.sidebar_button_force_pairall.configure(text="Pairing...",state="disabled")
         
         threading.Thread(target=lambda: app_instance.execute_subprocess(command, exe_path)).start()
-    
-        if command == "pairall":
-            self.after(5000, lambda: self.sidebar_button_pairall.configure(text="PairAll",state="normal"))
-        elif command == "forcepairall":
-            self.after(5000, lambda: self.sidebar_button_force_pairall.configure(text="ForcePairAll",state="normal"))
+        threading.Timer(5, delayed_function).start()
 
-        threading.Timer(5, app_instance.check_status).start()
         app_instance.insert_log("Executed command: " + command)
-
-    def sidebar_optionemenu_callback(self, new_appearance_mode: str):#Functions to change the theme
-        ctk.set_appearance_mode(new_appearance_mode)
 
 class ScrollableFrame(ctk.CTkScrollableFrame):
     def __init__(self, master, **kwargs):
@@ -91,94 +88,66 @@ class DeviceFrame(ctk.CTkFrame):
         super().__init__(master, **kwargs)
         self.app_instance = app_instance
 
-        self.device_label_id = ctk.CTkLabel(self, text="Device ID :")
+        self.device_label_id = ctk.CTkLabel(self, text="Device ID :", font=("Arial Bold", 12),text_color=("#696969", "#DCE4EE"))
         self.device_label_id.grid(row=0, column=0, padx=(20, 0), pady=20)
-        self.device_label_serial = ctk.CTkLabel(self, text=serial)  # Display serial here
+        self.device_label_serial = ctk.CTkLabel(self, text=serial, font=("Arial Bold", 12),text_color=("#696969", "#DCE4EE"))  # Display serial here
         self.device_label_serial.grid(row=0, column=1, padx=(5, 20), pady=20)
 
-        self.device_label_name = ctk.CTkLabel(self, text=self.app_instance.get_device_name(serial)) # Display device name here
+        self.device_label_name = ctk.CTkLabel(self, text=self.app_instance.get_device_name(serial), font=("Arial Bold", 12),text_color=("#696969", "#DCE4EE")) # Display device name here
         #self.device_label_name.grid(row=0, column=2, padx=20, pady=20)
-        self.device_label_name.place(x=210,y=20)
+        self.device_label_name.place(x=220,y=20)
 
-        self.device_button_pair = ctk.CTkButton(self,text="Pair",width=100,command=lambda: self.device_button_callback("pair",serial))
+        self.device_button_pair = ctk.CTkButton(self,state="disabled",text="Checking...", font=("Arial Bold", 12),width=100,command=lambda: self.device_button_callback("pair",serial))
         self.device_button_pair.grid(row=0, column=4, padx=(70, 10), pady=20)
-        self.device_button_unpair = ctk.CTkButton(self,text="Unpair",width=100,command=lambda: self.device_button_callback("unpair",serial))
+        self.device_button_unpair = ctk.CTkButton(self,text="Unpair", font=("Arial Bold", 12),width=100,command=lambda: self.device_button_callback("unpair",serial))
         self.device_button_unpair.grid(row=0, column=5, padx=(10, 10), pady=20)
-        self.device_button_reset = ctk.CTkButton(self,text="DongleReset",width=100, text_color=("gray10", "#DCE4EE"), fg_color="transparent", border_width=2, 
+        self.device_button_reset = ctk.CTkButton(self,text="DongleReset", font=("Arial Bold", 12),width=100, text_color=("#696969", "#DCE4EE"), fg_color="transparent",hover_color=("#696969","#808080"), 
+                                                 border_width=2, border_color=("#a9a9a9","#c0c0c0"),
                                                  command=lambda: self.device_button_callback("donglereset",serial))
         self.device_button_reset.grid(row=0, column=6, padx=(10, 20), pady=20)
 
     def device_button_callback(self,command,serial):#Processing when a button on the device frame is pressed
-        exe_path = self.app_instance.exe_path_frame.exepath_entry_serial.get()
+        exe_path = self.app_instance.get_exe_path()
         
         def check_exists(texts,states):
             if self.device_button_pair.winfo_exists():
                 self.device_button_pair.configure(text=texts,state=states)
-
+            
         if command == "pair":
             check_exists("Pairing...","disabled")
 
         threading.Thread(target=lambda: self.app_instance.execute_subprocess_serial(serial, command, exe_path)).start()
-        self.app_instance.insert_log("Executed command : " + serial +" "+ command)
-        
-        if command == "pair":
-            threading.Timer(5, lambda: check_exists("Pair","normal")).start()
-
-
         threading.Timer(5, self.app_instance.check_status).start()
+
+        self.app_instance.insert_log("Executed command : " + serial +" "+ command)
 
     def change_button_status(self, status):#Change the status of a button on the device frame
         if self.winfo_exists():
             if status == "normal":
                 self.device_button_pair.configure(state=status, text="Pair")
+                self.configure(fg_color=("#cecece","#323332"))
             elif status == "disabled":
                 self.device_button_pair.configure(state=status, text="Paired")
-
-class ExePathFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):#Widget Placement
-        super().__init__(master, **kwargs)
-
-        self.exepath_entry_serial = ctk.CTkEntry(self, width=120)
-        self.exepath_entry_serial.grid(row=0, column=0, padx=(20, 0), pady=(20, 20), sticky="ew")
-        self.exepath_button_browse = ctk.CTkButton(self,text="Browse",fg_color="transparent", text_color=("gray10", "#DCE4EE"), border_width=2,
-                                                   command=self.exepath_button_callback)
-        self.exepath_button_browse.grid(row=0, column=1, padx=20, pady=20)
-
-        #reset
-        self.exepath_entry_serial.delete(0, ctk.END)
-        self.exepath_entry_serial.insert(0, r"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\tools\lighthouse\bin\win64\lighthouse_console.exe")
-
-    def exepath_button_callback(self):#Processing when the browse button is pressed
-        app_instance = self.master
-        file_name = app_instance.read_file()
-
-        if file_name is not None:
-            self.exepath_entry_serial.delete(0, ctk.END)
-            self.exepath_entry_serial.insert(0, file_name)
-            threading.Thread(target=app_instance.check_exe).start()
-                  
+                self.configure(fg_color=("#b0b0b0","#696969"))
+                
 class App(ctk.CTk):
     def __init__(self):#Frame and widget placement
         super().__init__()
 
         #Window settings
-        ctk.set_appearance_mode("System")
         ctk.set_default_color_theme("blue")
         self.title("watchman_pairing_assistant")
-        self.after(201, lambda :self.iconbitmap(r'resources\icon.ico')) #set icon dir
-        self.geometry(f"{940}x{590}")
+        #self.after(201, lambda :self.iconbitmap(r'resources\icon.ico')) #set icon dir
+        self.geometry(f"{940}x{580}")
+        config = self.load_config()
+        ctk.set_appearance_mode(config.get("theme"))
         
         self.grid_rowconfigure(1, weight=1)
         self.grid_columnconfigure(1, weight=1)
 
-        self.exe_path_frame = ExePathFrame(self)
-        self.exe_path_frame.grid(row=0, column=1, padx=(20, 20), pady=(10, 0), sticky="nsew")
-        self.exe_path_frame.grid_rowconfigure(0, weight=1)
-        self.exe_path_frame.grid_columnconfigure(0, weight=1)
-
-        self.sidebar_frame = SidebarFrame(self)
+        self.sidebar_frame = SidebarFrame(self, app_instance=self)
         self.sidebar_frame.grid(row=0, column=0, rowspan=5, sticky="nsew")
-        self.sidebar_frame.grid_rowconfigure(6, weight=1)
+        self.sidebar_frame.grid_rowconfigure(8, weight=1)
 
         self.scrollable_frame = ScrollableFrame(self)
         self.scrollable_frame.grid(row=1, column=1, padx=(20, 20), pady=(10, 0), sticky="nsew")
@@ -188,8 +157,7 @@ class App(ctk.CTk):
         self.textbox_log.grid(row=2, column=1, padx=(20, 20), pady=(10, 20), sticky="nsew")
 
         #reset
-        self.insert_log("Welcome to watchman_pairing_assistant ! (v1.5)")
-        self.check_exe()
+        self.insert_log("Welcome to watchman_pairing_assistant ! (v2.0)")
         self.sidebar_frame.sidebar_button_reload_callback()
         
     def insert_log(self,log):#Functio to display logs in console and text box
@@ -219,7 +187,7 @@ class App(ctk.CTk):
                 process.wait()
 
     def check_status(self):#Function to determine dongle connection status
-        exe_path = self.exe_path_frame.exepath_entry_serial.get()
+        exe_path = self.get_exe_path()
         output = self.execute_subprocess("dongleinfo", exe_path)
         #print(output)
 
@@ -251,31 +219,40 @@ class App(ctk.CTk):
             return "IndexHMD"
         elif serial_number.endswith(("LYX")):   #Dongles made with Index firmware when the serial number ends with "LYX"
             return "IndexFW"
-        elif serial_number.endswith(("DYX")):   #If the serial ends with DYX, it is an etee dongle
-            return "etee"
+        elif serial_number.endswith(("DYX")):   #If the serial ends with DYX, it is dongles made with nrf52840 (etee, Shiftall, etc.)
+            return "IndexFW"
         elif re.match(r".*(-[0-9]YX)$", serial_number): 
             #If the last part of the serial number begins with a "-" followed by a single digit and ends with "YX", it is a Tundra labs Super Wireless Dongle
             return "Tundra"
         else:   #Other dongles that are not identifiable (vive HMD or general nrf24 dongles)
             return "Dongle" 
-        
-    def check_exe(self):#Function to determine if the selected exe is a lighthouse_console.exe
-        exe_path = self.exe_path_frame.exepath_entry_serial.get()
-        output = self.execute_subprocess("serial", exe_path)
-        #print(output)
 
-        if "lighthouse_console.exe" in output:  #Judging by the presence of "lighthouse_console.exe" in the output results
-            self.insert_log("Successfully recognized lighthouse_console.exe")
-        else:
-            self.insert_log("Failed to recognize lighthouse_console.exe")
+    def get_exe_path(self):#Get exe path
+        config = self.load_config()
+        exe_path = config.get("lighthouse_console_path")
+        return exe_path
 
-    def read_file(self):#Function to display a file dialog
-        file_path = ctk.filedialog.askopenfilename(filetypes=[("Executable Files", "*.exe")])
+    def load_config(self):  # Generating and getting json
+        config_path = os.path.join(os.path.dirname(sys.argv[0]), "resources", "config.json")
+        resources_dir = os.path.join(os.path.dirname(sys.argv[0]), "resources")
 
-        if len(file_path) != 0:
-            return file_path
-        else:
-            return None
+        default_config = {
+            "theme": "Dark",
+            "lighthouse_console_path": r"C:\Program Files (x86)\Steam\steamapps\common\SteamVR\tools\lighthouse\bin\win64\lighthouse_console.exe"
+        }
+
+        if not os.path.exists(resources_dir):
+            os.makedirs(resources_dir, exist_ok=True)
+
+        if not os.path.exists(config_path):
+            with open(config_path, "w") as config_file:
+                json.dump(default_config, config_file)
+
+        if os.path.exists(config_path):
+            with open(config_path, "r") as config_file:
+                config = json.load(config_file)
+
+        return config
 
 if __name__ == "__main__":
     app = App() 
